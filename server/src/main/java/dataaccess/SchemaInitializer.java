fdd191432f6a54a2b90d4e0597b551f64a0459e0
PorterGrigg <pag37@byu.edu>
2026-03-11 13:51:32 -0600

    Phase 4 Implementing DataBase Schema Tables

--- server/src/main/java/dataaccess/SchemaInitializer.java
+++ server/src/main/java/dataaccess/SchemaInitializer.java
@@ -7,42 +7,40 @@ public class SchemaInitializer {
 
     private static final String[] createAuthDataStatement = {
             """
-        CREATE TABLE IF NOT EXISTS  pet (
-          `id` int NOT NULL AUTO_INCREMENT,
-          `name` varchar(256) NOT NULL,
-          `type` ENUM('CAT', 'DOG', 'FISH', 'FROG', 'ROCK') DEFAULT 'CAT',
-          `json` TEXT DEFAULT NULL,
-          PRIMARY KEY (`id`),
-          INDEX(type),
-          INDEX(name)
+        CREATE TABLE IF NOT EXISTS  AuthData (
+          `authToken` varchar(256) NOT NULL,
+          `username` varchar(256) NOT NULL,
+          PRIMARY KEY (`authToken`),
+          INDEX(username),
         ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
         """
-    };
+    }; //index allows faster queries //Last line specifies the engine we use in SQL, the set of characters, and the way characters are sorted
 
     private static final String[] createUserDataStatement = {
             """
-        CREATE TABLE IF NOT EXISTS  pet (
-          `id` int NOT NULL AUTO_INCREMENT,
-          `name` varchar(256) NOT NULL,
-          `type` ENUM('CAT', 'DOG', 'FISH', 'FROG', 'ROCK') DEFAULT 'CAT',
-          `json` TEXT DEFAULT NULL,
-          PRIMARY KEY (`id`),
-          INDEX(type),
-          INDEX(name)
+        CREATE TABLE IF NOT EXISTS  UserData (
+          `username` varchar(256) NOT NULL,
+          `password` varchar(256) NOT NULL,
+          `email` varchar(256) NOT NULL,
+          PRIMARY KEY (`username`),
+          INDEX(password),
+          INDEX(email)
         ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
         """
     };
 
     private static final String[] createGameDataStatement = {
             """
-        CREATE TABLE IF NOT EXISTS  pet (
-          `id` int NOT NULL AUTO_INCREMENT,
-          `name` varchar(256) NOT NULL,
-          `type` ENUM('CAT', 'DOG', 'FISH', 'FROG', 'ROCK') DEFAULT 'CAT',
-          `json` TEXT DEFAULT NULL,
-          PRIMARY KEY (`id`),
-          INDEX(type),
-          INDEX(name)
+        CREATE TABLE IF NOT EXISTS  GameData (
+          `gameID` int NOT NULL AUTO_INCREMENT,
+          `whiteUsername` varchar(256),
+          `blackUsername` varchar(256),
+          `gameName` varchar(256) NOT NULL,
+          `game` TEXT DEFAULT NULL,
+          PRIMARY KEY (`gameID`),
+          INDEX(whiteUsername),
+          INDEX(blackUsername),
+          INDEX(gameName)
         ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
         """
     };
