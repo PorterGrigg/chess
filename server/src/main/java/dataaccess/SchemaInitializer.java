72d7e52046f8c9ef5c85f43df300ef0d29c46231
PorterGrigg <pag37@byu.edu>
2026-03-11 11:07:24 -0600

    Phase 4 Implementing DataBase Schema Tables

--- server/src/main/java/dataaccess/SchemaInitializer.java
+++ server/src/main/java/dataaccess/SchemaInitializer.java
@@ -1,4 +1,73 @@
 package dataaccess;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class SchemaInitializer {
+
+    private static final String[] createAuthDataStatement = {
+            """
+        CREATE TABLE IF NOT EXISTS  pet (
+          `id` int NOT NULL AUTO_INCREMENT,
+          `name` varchar(256) NOT NULL,
+          `type` ENUM('CAT', 'DOG', 'FISH', 'FROG', 'ROCK') DEFAULT 'CAT',
+          `json` TEXT DEFAULT NULL,
+          PRIMARY KEY (`id`),
+          INDEX(type),
+          INDEX(name)
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+        """
+    };
+
+    private static final String[] createUserDataStatement = {
+            """
+        CREATE TABLE IF NOT EXISTS  pet (
+          `id` int NOT NULL AUTO_INCREMENT,
+          `name` varchar(256) NOT NULL,
+          `type` ENUM('CAT', 'DOG', 'FISH', 'FROG', 'ROCK') DEFAULT 'CAT',
+          `json` TEXT DEFAULT NULL,
+          PRIMARY KEY (`id`),
+          INDEX(type),
+          INDEX(name)
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+        """
+    };
+
+    private static final String[] createGameDataStatement = {
+            """
+        CREATE TABLE IF NOT EXISTS  pet (
+          `id` int NOT NULL AUTO_INCREMENT,
+          `name` varchar(256) NOT NULL,
+          `type` ENUM('CAT', 'DOG', 'FISH', 'FROG', 'ROCK') DEFAULT 'CAT',
+          `json` TEXT DEFAULT NULL,
+          PRIMARY KEY (`id`),
+          INDEX(type),
+          INDEX(name)
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+        """
+    };
+
+    public static void createTables() throws DataAccessException{
+        try (
+                Connection conn = DatabaseManager.getConnection()) {
+            for (String statement : createAuthDataStatement) {
+                try (var preparedStatement = conn.prepareStatement(statement)) {
+                    preparedStatement.executeUpdate();
+                }
+            }
+            for (String statement : createUserDataStatement) {
+                try (var preparedStatement = conn.prepareStatement(statement)) {
+                    preparedStatement.executeUpdate();
+                }
+            }
+            for (String statement : createGameDataStatement) {
+                try (var preparedStatement = conn.prepareStatement(statement)) {
+                    preparedStatement.executeUpdate();
+                }
+            }
+        } catch (
+                SQLException ex) {
+            //throw new DataAccessException(DataAccessException.Code.ServerError, String.format("Unable to configure database: %s", ex.getMessage()));
+        }
+    }
 }
