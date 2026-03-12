fc24dbc258f3f798d2715e4b9bd09cf4ce954231
PorterGrigg <pag37@byu.edu>
2026-03-11 23:59:08 -0600

    Phase 4 First DAO Tests

--- server/src/test/java/dataaccess/DataAccessTests.java
+++ server/src/test/java/dataaccess/DataAccessTests.java
@@ -7,6 +7,7 @@ import dataaccess.MemoryAuthDAO;
 import dataaccess.MemoryGameDAO;
 import dataaccess.MemoryUserDAO;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName; //I was trying to match the provided chesssboard tests
 import org.junit.jupiter.api.Test;
 import model.AuthData;
@@ -15,6 +16,7 @@ import model.UserData;
 
 import requests.*;
 import results.*;
+import service.ClearService;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,5 +24,62 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class DataAccessTests {
+    private SQLAuthDAO authDAO;
+    private SQLUserDAO userDAO;
+    private SQLGameDAO gameDAO;
+
+    @BeforeEach
+    public void setUp() throws DataAccessException {
+        authDAO = new SQLAuthDAO();
+        userDAO = new SQLUserDAO();
+        gameDAO = new SQLGameDAO();
+
+        // clear all tables
+        ClearService clearService = new ClearService(authDAO, userDAO, gameDAO);
+        clearService.clearAll();
+    }
+
+    //Auth Tests
+    @Test
+    @DisplayName("AuthDAO Clear Positive")
+    public void clearAuthPositiveTest() throws DataAccessException {
+        authDAO.create(new AuthData("auth1", "porker"));
+
+        List<AuthData> authBefore = authDAO.readAll();
+        assertFalse(authBefore.isEmpty());
+
+        authDAO.clear();
+
+        assertTrue(authDAO.readAll().isEmpty());
+    }
+
+    //User Tests
+    @Test
+    @DisplayName("UserDAO Clear Positive")
+    public void clearUserPositiveTest() throws DataAccessException {
+        userDAO.create(new UserData("porker", "password", "porker@byu.edu"));
+
+        List<UserData> userBefore = userDAO.readAll();
+        assertFalse(userBefore.isEmpty());
+
+        userDAO.clear();
+
+        assertTrue(userDAO.readAll().isEmpty());
+    }
+
+    //Game Tests
+    @Test
+    @DisplayName("GameDAO Clear Positive")
+    public void clearGamePositiveTest() throws DataAccessException {
+        gameDAO.create(new GameData(10, "porker", "piggy",
+                "game1", null));
+
+        List<GameData> gameBefore = gameDAO.readAll();
+        assertFalse(gameBefore.isEmpty());
+
+        gameDAO.clear();
+
+        assertTrue(gameDAO.readAll().isEmpty());
+    }
 
 }
