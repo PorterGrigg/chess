80846ac7e95b34d8c2b337cd93f66208bdd165f0
PorterGrigg <pag37@byu.edu>
2026-03-24 15:47:24 -0600

    Phase 5 Start Implementing ServerFacade

---
+++ shared/src/main/java/results/ListResult.java
@@ -0,0 +1,8 @@
+package results;
+
+import model.GameData;
+
+import java.util.ArrayList;
+
+public record ListResult(ArrayList<GameData> games) {
+} //will the way game data automatically prints out match the specifications?
